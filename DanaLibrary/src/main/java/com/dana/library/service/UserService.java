8ecc8185491a0aa1a0133434f58b4127d56d7636
SeongHyeok <shhong497@gmail.com>
2024-01-23 12:10:07 +0900

    [수정]User Service, UserController + DTO, JS

--- DanaLibrary/src/main/java/com/dana/library/service/UserService.java
+++ DanaLibrary/src/main/java/com/dana/library/service/UserService.java
@@ -2,7 +2,10 @@ package com.dana.library.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dana.library.domain.Status;
+import com.dana.library.domain.User;
 import com.dana.library.persistence.UserRepository;
 
 @Service
@@ -10,5 +13,19 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Transactional
+	public void insertUser(User user) {
+		user.setUserStatus(Status.ACTIVE);
+		userRepository.save(user);
+	}
+	
+	@Transactional(readOnly = true)
+	public User getUser(int userNum) {
+		User findUser = userRepository.findById(userNum).orElseGet(()->{
+			return new User();
+		});
+		return findUser;
+	}
 
 }
