f5b0ed0961f1bd6c82613e2453cc6f48f111da1c
SeongHyeok <shhong497@gmail.com>
2024-01-24 14:53:25 +0900

    회원가입 페이지, 컨트롤러, js파일

--- DanaLibrary/src/main/java/com/dana/library/persistence/UserRepository.java
+++ DanaLibrary/src/main/java/com/dana/library/persistence/UserRepository.java
@@ -1,5 +1,7 @@
 package com.dana.library.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.dana.library.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+	Optional<User> findByUserid(String Userid);
 
 }
--- DanaLibrary/src/main/java/com/dana/library/service/UserService.java
+++ DanaLibrary/src/main/java/com/dana/library/service/UserService.java
@@ -2,7 +2,10 @@ package com.dana.library.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dana.library.domain.Status;
+import com.dana.library.domain.User;
 import com.dana.library.persistence.UserRepository;
 
 @Service
@@ -10,5 +13,22 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Transactional
+	public void insertUser(User user) {
+		user.setUserStatus(Status.ACTIVE);
+		userRepository.save(user);
+	}
+	
+	@Transactional(readOnly = true)
+	public User getUser(String userid) {
+		User findUser = userRepository.findByUserid(userid).orElseGet(()->{
+			return new User();
+		});
+		
+		return findUser;
+	}
+	
+	
 
 }
