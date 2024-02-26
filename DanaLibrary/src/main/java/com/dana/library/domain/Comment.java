fc5172bcc1d1d689f1b387725723d238aa29faa0
Danbi <danbik77@gmail.com>
2024-02-26 17:58:15 +0900

    게시판 수정

--- DanaLibrary/src/main/java/com/dana/library/domain/Comment.java
+++ DanaLibrary/src/main/java/com/dana/library/domain/Comment.java
@@ -7,7 +7,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
